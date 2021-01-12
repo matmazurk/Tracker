fbf3668915f1ec7ae048885e071dde8efbe88873
mat <mat.mazurk@gmail.com>
2021-01-12 19:26:11 +0100

    [code][UI] MainActivity renamed to TrackerActiviy, binding added

---
+++ app/src/main/java/com/mat/tracker/TrackerActivity.kt
@@ -0,0 +1,16 @@
+package com.mat.tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mat.tracker.databinding.ActivityTrackerBinding
+
+class TrackerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTrackerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTrackerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
