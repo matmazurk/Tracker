150a0dc6fec8ff53c3bbba2eb228d3573210fa2d
mat <mat.mazurk@gmail.com>
2021-01-14 16:56:09 +0100

    [code] database and LocationManager entered to koin module

--- app/src/main/java/com/mat/tracker/KoinModule.kt
+++ app/src/main/java/com/mat/tracker/KoinModule.kt
@@ -1,7 +1,19 @@
 package com.mat.tracker
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
 
+    single { Room.databaseBuilder(
+            androidContext(),
+            TrackerDatabase::class.java,
+            "tracker-db"
+        ).build()
+    }
+
+    single {
+        LocationManager(androidContext())
+    }
 }
