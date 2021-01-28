de52f912d52986432ad7cd91fbeff0e3e4317c10
mat <mat.mazurk@gmail.com>
2021-01-28 13:43:34 +0100

    [code] changed implementation in location manager

--- app/src/main/java/com/mat/tracker/LocationManager.kt
+++ app/src/main/java/com/mat/tracker/LocationManager.kt
@@ -1,24 +1,62 @@
 package com.mat.tracker
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import androidx.annotation.MainThread
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import java.util.concurrent.TimeUnit
 
 class LocationManager(private val context: Context) {
 
-    fun getUpdateLocation(block: (Location?) -> Unit) {
+    private val _receivingLocationUpdates: MutableLiveData<Boolean> = MutableLiveData(false)
+    val receivingLocationUpdates: LiveData<Boolean> get() = _receivingLocationUpdates
 
-        val fineLocationPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+    private val fusedLocationClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(context)
 
-        if (fineLocationPermission == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.getFusedLocationProviderClient(context).lastLocation.addOnSuccessListener {
-                block.invoke(it)
-            }
-        } else {
-            block.invoke(null)
+    private val locationRequest: LocationRequest = LocationRequest().apply {
+        interval = TimeUnit.SECONDS.toMillis(INTERVAL_DURATION_SECONDS)
+        fastestInterval = TimeUnit.SECONDS.toMillis(FASTEST_INTERVAL_DURATION_SECONDS)
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    private val locationUpdatePendingIntent: PendingIntent by lazy {
+        TODO()
+    }
+
+    @Throws(SecurityException::class)
+    @MainThread
+    fun startLocationUpdates() {
+
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) return
+
+        try {
+            _receivingLocationUpdates.value = true
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationUpdatePendingIntent)
+        } catch (permissionRevoked: SecurityException) {
+            _receivingLocationUpdates.value = false
+            throw permissionRevoked
         }
+    }
+
+    @MainThread
+    fun stopLocationUpdates() {
+        _receivingLocationUpdates.value = false
+        fusedLocationClient.removeLocationUpdates(locationUpdatePendingIntent)
+    }
+
+    companion object {
+        private const val INTERVAL_DURATION_SECONDS = 60L
+        private const val FASTEST_INTERVAL_DURATION_SECONDS = 30L
     }
 }
