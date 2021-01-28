40a9e5f8c0189d836a4e21bbc6b835cd15579a51
mat <mat.mazurk@gmail.com>
2021-01-28 13:57:04 +0100

    [code] added insert locations, changed returning type for all locations to flow

--- app/src/main/java/com/mat/tracker/LocationsDao.kt
+++ app/src/main/java/com/mat/tracker/LocationsDao.kt
@@ -4,14 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface LocationsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertLocation(location: LocationData)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLocations(locations: List<LocationData>)
+
     @Query("SELECT * FROM LocationData")
-    suspend fun getAllLocations() : List<LocationData>
+    fun getAllLocations() : Flow<List<LocationData>>
 
     @Query("DELETE FROM LocationData")
     suspend fun nukeTable()
